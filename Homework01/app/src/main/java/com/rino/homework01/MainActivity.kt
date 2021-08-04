2713bfc23555065ed45945850bfbaca308289779
Rinat <mr.rino@gmail.com>
2021-08-04 13:36:05 +0300

    Added a button and set on click listener

--- Homework01/app/src/main/java/com/rino/homework01/MainActivity.kt
+++ Homework01/app/src/main/java/com/rino/homework01/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.rino.homework01
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,6 +10,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val clickMeButton = findViewById<Button>(R.id.click_me_button)
+        val infoTextView = findViewById<TextView>(R.id.info_text_view)
+
+        clickMeButton.setOnClickListener {
+            infoTextView.text = getString(R.string.button_was_clicked)
+        }
     }
 
 }
