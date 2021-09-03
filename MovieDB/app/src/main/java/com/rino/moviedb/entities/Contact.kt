84bbe15947c0ce8d0a2d3e4f31db0ecd7211bf6b
Rinat <mr.rino@gmail.com>
2021-09-03 19:00:04 +0300

    Small fix with contact displayName

--- MovieDB/app/src/main/java/com/rino/moviedb/entities/Contact.kt
+++ MovieDB/app/src/main/java/com/rino/moviedb/entities/Contact.kt
@@ -2,7 +2,7 @@ package com.rino.moviedb.entities
 
 data class Contact(
     val id: String,
-    val displayName: String,
+    val displayName: String?,
     val photoThumbUri: String?,
     val hasPhoneNumber: Boolean,
     val phoneNumber: String?
