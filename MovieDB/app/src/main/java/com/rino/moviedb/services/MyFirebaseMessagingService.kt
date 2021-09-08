4a60be6cff4e3ff6271cff5efee3e74ce8436d1b
Rinat <mr.rino@gmail.com>
2021-09-08 14:16:15 +0300

    Added FirebaseMessaging in the project

---
+++ MovieDB/app/src/main/java/com/rino/moviedb/services/MyFirebaseMessagingService.kt
@@ -0,0 +1,22 @@
+package com.rino.moviedb.services
+
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    companion object {
+        private const val TAG = "MyFirebaseMessagingService"
+    }
+
+    override fun onNewToken(token: String) {
+        Log.d(TAG, "Refreshed token: $token")
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // FCM registration token to your app server.
+        // sendRegistrationToServer(token)
+    }
+    
+}
